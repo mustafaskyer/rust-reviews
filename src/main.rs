a2b838258e51f75880a703ba4fc8555720596ac4
Mustafa Elsayed <mustafaskyer@gmail.com>
2026-03-08 11:40:31 +0300

    heap & stack with primitives

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
-    let mut message = "Welcome there";
-    message = "Hello World from Rust";
-    println!("{}", message);
+    let name = String::from("John Doe"); // memory allocation
+    let name2 = name.clone(); // need to clone
+    println!("Name1 is {}, and Name 2: {}", name, name2);
+    
+    let char1 = '1'; // stack allocation
+    let char2 = char1; // no need to clone
+    println!("char1 is {}, char2 is {}", char1, char2);
 }
